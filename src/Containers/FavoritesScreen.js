78c41421d9d973eca0c363f17bc1b5278630eca5
Ivan Diniz <ivan@roadmaps.com.br>
2020-04-10 20:15:22 -0300

    WIP: create component favorites card

--- src/Containers/FavoritesScreen.js
+++ src/Containers/FavoritesScreen.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import institutions from '../db/institutions.json'
 
 export default function Favorites() {
+
+  const renderInstitutions = () => {
+    return (
+      institutions.map((institution, key) => {
+        return (
+          <section key={key} className="card-primary">
+
+            <figure>
+              <img src={institution.photos[0]} alt={institution.name} />
+            </figure>
+
+            <div className="card-primary-content">
+              <div className="card-primary-header">
+                <h3>{institution.name}</h3>
+              </div>
+              <hr className="card-primary-line" />
+              <div className="card-primary-footer">
+                <p className="card-primary-text">{institution.phone[0] || ''}</p>
+                <div className="card-primary-badge">123km</div>
+              </div>
+            </div>
+
+          </section>
+        )
+      })
+    )
+  }
+
   return (
     <div className="view">
       <div className="content">
         <div className="content--inner">
-          Tela de eventos
+          {renderInstitutions()}
         </div>
       </div>
     </div>
