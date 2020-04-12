8056054f268312ce4377b1525db45a0f95a17a73
Ivan Diniz <ivan@roadmaps.com.br>
2020-04-11 23:23:53 -0300

    feat: base to component institution

--- src/Containers/InstitutionScreen.js
+++ src/Containers/InstitutionScreen.js
@@ -1,15 +1,65 @@
 import React, { useState, useEffect } from 'react'
+import Line from '../Components/LineComponent'
 
 import categoriesDb from '../db/categories.json'
-import { useHistory, useParams } from "react-router-dom"
+import { useHistory } from 'react-router-dom'
+import ButtonBack from '../Components/ButtonBackComponent'
 
-export default function Institution() {
+export default function Institution(props) {
 
-  let { id } = useParams();
+  const [institution, setInstitution] = useState({})
+  let history = useHistory()
 
+  useEffect(() => {
+    setInstitution(props.institution)
+  }, [])
+
+  const handleBack = () => {
+    props.openInstitution({})
+  }
+
+  const renderCategories = (categories) => {
+    const categoriesFilters = categoriesDb.filter(category =>
+      categories.find(f => f === category.value)
+    )
+
+    if (categoriesFilters.length > 0) {
+      return (
+        <div>{
+          categoriesFilters.map((category, key) => {
+            return (
+              <div key={key}>
+                {category.value}
+              </div>
+            )
+          })
+        }
+        </div>
+      )
+    } else {
+      return <i>Nada para doar :( </i>
+    }
+  }
   return (
     <div className="view">
-      <h2>Instituições {id}</h2>
+      <header>
+        <section>
+          <ButtonBack onClick={handleBack} />
+          <h3>{institution.name}</h3>
+          <Line />
+          <p>{institution.description}</p>
+        </section>
+      </header>
+      <section className="container">
+        <p>{institution.address}</p>
+        <Line />
+        <p>{institution.phone && (institution.phone)}</p>
+        <Line />
+        <h4>Intens em Falta</h4>
+        <div>
+          {institution.categories && (renderCategories(institution.categories))}
+        </div>
+      </section>
     </div>
   )
 }
