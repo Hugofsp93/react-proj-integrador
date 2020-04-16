18b4ab59f146475e8b404d6b3e23b046418877d0
Ivan Diniz <ivan@roadmaps.com.br>
2020-04-16 16:59:28 -0300

    fix: path image

--- src/Containers/SplashScreen.js
+++ src/Containers/SplashScreen.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-export default function Splash(props) {
+import React from 'react';
+import { Images } from '../Utils';
+export default function Splash() {
 
   return (
     <div className="splash">
       <div className="splash--image">
-        <img src="/images/logo.png" />
+        <img src={Images.path('logo.png')} />
       </div>
       <div className="splash--version">
         <p>version 1.2.0</p>
